00b2c675a3ffd02d7dd570cd22922cc496de5556
ngurahchandraa <ngurahchandra@student.telkomuniversity.ac.id>
2024-08-27 23:31:12 +0800

    Get All Orders

--- app/src/main/java/com/example/mebelcart/data/order/Order.kt
+++ app/src/main/java/com/example/mebelcart/data/order/Order.kt
@@ -2,11 +2,16 @@ package com.example.mebelcart.data.order
 
 import com.example.mebelcart.data.Address
 import com.example.mebelcart.data.CartProduct
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.random.Random.Default.nextLong
 
 data class Order (
-    val orderStatus: String,
-    val totalPrice: Float,
-    val products: List<CartProduct>,
-    val address: Address
+    val orderStatus: String = "",
+    val totalPrice: Float = 0f,
+    val products: List<CartProduct> = emptyList(),
+    val address: Address = Address(),
+    val date : String = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(Date()),
+    val orderId: Long = nextLong(0,100_000_000_000) + totalPrice.toLong()
 
         )
